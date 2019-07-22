0d70b3c95e2a9e993327fd1e056542cb84e96636
Jonathan Baker <ssorpg@gmail.com>
2019-07-22 15:16:10 -0700

    mostly complete, TODO bugfixing

---
+++ app/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const PORT = 8080;
+
+var friendsList = require('./data/friends');
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/survey.html');
+});
+
+app.get('/api/friends', (req, res) => {
+    res.json(friendsList);
+});
+
+app.post('/api/friends', (req, res) => {
+    let scoreResults = [];
+
+    for (let i = 0; i < friendsList.length; i++) {
+        const friend = friendsList[i];
+        
+        let scoreResult = 0;
+
+        for (let j = 0; j < friend.scores.length; j++) {
+            scoreResult += Math.abs(friend.scores[j] - req.body.scores[j]);
+        }
+
+        scoreResults.push(scoreResult);
+    }
+
+    let curMin = 50;
+    let closestMatch = 0;
+
+    for (let i = 0; i < scoreResults.length; i++) {
+        const curScore = scoreResults[i];
+        
+        if (curScore < curMin) {
+            curMin = curScore;
+            closestMatch = i;
+        }
+    }
+
+    friendsList.push(req.body);
+
+    res.json(friendsList[closestMatch]);
+});
+
+app.listen(PORT, () => {
+    console.log('App listening on PORT ' + PORT);
+});
